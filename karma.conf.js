e41cf7ace915115b134a63b80a17a3a12bc349a9
horrorsheep <ruanziyang@gmail.com>
2019-08-07 10:16:53 -0700

    initial deploy

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,13 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browsers: ['Chrome_without_sandbox'],
+    customLaunchers: {
+      Chrome_without_sandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'] // with sandbox it fails under Docker
+      }
+    },
   });
 };
